f4e5a504869d6c534f0382e48c4b2b52485c3cc8
shaw342 <shawanbarua70@gmail.com>
2024-07-18 22:53:14 +0200

    add GET user and GET project route

--- backend /main.go
+++ backend /main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	
-
-
-	"github.com/fauna/fauna-go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	//"github.com/shaw342/projet_argile/backend/Verification"
-	"github.com/shaw342/projet_argile/backend/model"
 	"github.com/shaw342/projet_argile/backend/repository/Fauna"
-	//"github.com/shaw342/projet_argile/backend/repository"
 )
 
-
-var client *fauna.Client
-var clientERR error
 
 func main() {
 	r := gin.New()
@@ -26,44 +17,10 @@ func main() {
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
 	r.Use(cors.New(config))
 
-	client, clientERR = fauna.NewDefaultClient()
-
-	if clientERR != nil {
-		panic(clientERR)
-	}
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, map[string]any{
 			"hello": "world",
 		})
-	})
-
-	r.GET("/project/get", func(ctx *gin.Context) {
-		data := model.Project{}
-
-		data.Id = "123e4567-e89b-12d3-a456-426614174000"
-		data.Name = "my first task"
-		data.Owner = "654f3210-feda-4baf-8765-081235432100"
-
-		ctx.JSON(200, data)
-	})
-
-	r.GET("/task/get", func(ctx *gin.Context) {
-		data := model.Task{}
-
-		data.Id = "123e4567-e89b-12d3-a456-426614174000"
-		data.Name = "create user interface"
-		data.State = "INPROGRESS"
-
-		ctx.JSON(200, data)
-	})
-
-	r.GET("/user/get", func(ctx *gin.Context) {
-		data := model.User{}
-
-		data.Id = "123e4567-e89b-12d3-a456-426614174000"
-		data.Name = "shawan"
-
-		ctx.JSON(200, data)
 	})
 
 	v1 := r.Group("api/v1")
@@ -71,12 +28,14 @@ func main() {
 		v1.POST("/user", repository.CreateUser)
 		v1.POST("/task", repository.CreateTask)
 		v1.POST("/project", repository.CreateProject)
-		v1.POST("/login",repository.login)
 		v1.DELETE("/deleteProject",repository.DeleteProject)
 		v1.DELETE("/deleteTask",repository.DeleteTask)
 		v1.PATCH("/updateProject",repository.UpdateProject)
 		v1.PATCH("/updateTask",repository.UpdateTasks)
-		v1.GET("/getTask",repository.GetTask) 
+		v1.GET("/task/get",repository.GetTask)
+		v1.GET("/project/get",repository.GetProject)
+		v1.GET("/user/get",repository.GetUser)
+
 	}
 	r.Run()
 }
